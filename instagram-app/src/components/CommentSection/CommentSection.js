2cd233805dc447dccd45112d50bb1af38c7a7fc9
cwern40 <chris.wernli@hotmail.com>
2019-06-12 19:51:10 -0600

    I finished the comment section and have all of the information rendering to the screen. I created the css page and started styling the posts

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Comment = (props) => {
     console.log('Comment', props);
     return (
         <div className="comment-section">
-            comments
+            {props.comment.map(comment => (
+                <div key={comment.id} className="comment">
+                    <p className="user">
+                        {comment.username}
+                    </p>
+                    <p className="comment-text">
+                        {comment.text}
+                    </p>
+                </div>
+            ))}
         </div>
     );
+};
+
+Comment.propTypes = {
+    comment: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            username: PropTypes.string,
+            text: PropTypes.string
+        })
+    )
 }
 
 export default Comment;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,31 @@
 import React from "react";
+import Comment from '../CommentSection/CommentSection';
+import './Post.css';
 
 const PostContainer = (props) => {
     console.log('Post Container', props);
     return (
         <div className='post-container'>
+            {props.post.map(post => (
+                <>
+                <div key={post.id} className="post">
+                    <div className="user-id">
+                        <img src={post.thumbnailUrl} />
+                        <p>{post.username}</p>
+                    </div>
+                    <div className="image">
+                        <img src={post.imageUrl} />
+                    </div>
+                    <div className="likes">
+                        <div className="like-icons">
 
+                        </div>
+                        <p>{post.likes}</p>
+                    </div>
+                </div>
+                <Comment comment={post.comments} />
+                </>
+            ))}
         </div>
     );
 }
