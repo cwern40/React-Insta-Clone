ab8b32c22cead7fa654574dae025f548371cf53d
cwern40 <chris.wernli@hotmail.com>
2019-06-15 10:53:26 -0600

    I added the comment input section

---
+++ instagram-app/src/components/CommentSection/CommentInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CommentInput = (props) => {
+    return (<form>
+        <input type="text" placeholder="Add Comment..." />
+    </form>
+    );
+};
+
+export default CommentInput;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -5,26 +5,26 @@ import './Post.css';
 const PostContainer = (props) => {
     console.log('Post Container', props);
     return (
-        <div className='post-container'>
+        <div className='all-posts'>
             {props.post.map(post => (
-                <>
+                <div className='post-container'>
                 <div key={post.id} className="post">
                     <div className="user-id">
-                        <img src={post.thumbnailUrl} />
+                        <img src={post.thumbnailUrl} alt="User profile" />
                         <p>{post.username}</p>
                     </div>
                     <div className="image">
-                        <img src={post.imageUrl} />
+                        <img src={post.imageUrl} alt="Post" />
                     </div>
                     <div className="likes">
                         <div className="like-icons">
 
                         </div>
-                        <p>{post.likes}</p>
+                        <p>{post.likes} likes</p>
                     </div>
                 </div>
                 <Comment comment={post.comments} />
-                </>
+                </div>
             ))}
         </div>
     );
